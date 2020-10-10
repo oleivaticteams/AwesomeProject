aabd5219706068d2a0cd89b3ea7ffa681806633f
Omar Leiva <omar.e.leiva@gmail.cojm>
2020-10-09 19:34:45 -0500

    cambios

--- src/Pantallas/AddProduct.js
+++ src/Pantallas/AddProduct.js
@@ -2,15 +2,27 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Input, Button } from "react-native-elements";
 
+import { addRegistro } from "../Utils/Acciones";
+
 export default function AddProduct() {
   const [nombre, setnombre] = useState("");
   const [descripcion, setdescripcion] = useState("");
   const [precio, setprecio] = useState(0);
 
-  const onSubmit = () => {
+  const onSubmit = async () => {
     console.log("El nombre es " + nombre);
     console.log("La Descripción es " + descripcion);
     console.log("El Precio es " + precio);
+
+    const producto = {
+      nombre,
+      descripcion,
+      precio,
+    };
+
+    const resultado = await addRegistro("Productos", producto);
+
+    console.log(resultado);
   };
 
   return (
