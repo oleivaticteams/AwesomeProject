bbadf39c8ee221d718056b0b9f6ec1b04be8b8e1
Omar Leiva <omar.e.leiva@gmail.cojm>
2020-10-09 19:25:36 -0500

    AddRegistro

--- src/Utils/Acciones.js
+++ src/Utils/Acciones.js
@@ -1,8 +1,26 @@
 import { firebaseapp } from "./Firebase";
 import * as firebase from "firebase";
+import "firebase/firestore";
+const db = firebase.firestore(firebaseapp);
 
 export const validarSesion = () => {
   firebase.auth().onAuthStateChanged((user) => {
     user ? console.log("ok") : console.log("Iniciar sesión");
   });
+};
+
+export const addRegistro = async (coleccion, campos) => {
+  let resultado = false;
+
+  await db
+    .collection(coleccion)
+    .add(campos)
+    .then((response) => {
+      resultado = true;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  return resultado;
 };
