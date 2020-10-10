c8a2edf1e2d1633c0695fb3900948d163c35d36f
Omar Leiva <omar.e.leiva@gmail.cojm>
2020-10-09 19:50:03 -0500

    prueba

--- App.js
+++ App.js
@@ -2,9 +2,17 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import ComponentePrueba from "./ComponentePrueba";
-
 import { validarSesion } from "./src/Utils/Acciones";
 import AddProduct from "./src/Pantallas/AddProduct";
+import { decode, encode } from "base-64";
+
+if (!global.btoa) {
+  global.btoa = encode;
+}
+
+if (!global.atob) {
+  global.atob = decode;
+}
 
 export default function App() {
   validarSesion();
